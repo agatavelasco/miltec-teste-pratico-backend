9223eb294e0fb1c4bc50cac182c6308a0f5a5e8d
Ágata Velasco Penze <avpenze@gmail.com>
2025-01-31 11:58:00 -0300

    fix: persistence resolve

--- MiltecApi/Data/AppDbContext.cs
+++ MiltecApi/Data/AppDbContext.cs
@@ -5,18 +5,8 @@ namespace MiltecApi.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Anuncio> Anuncios { get; set; }
-
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Anuncio>()
-                .Property(a => a.Valor)
-                .HasColumnType("decimal(18,2)");
-        }
-
+        public DbSet<Anuncio> Anuncios { get; set; }
     }
 }
--- MiltecApi/Program.cs
+++ MiltecApi/Program.cs
@@ -4,25 +4,20 @@ using MiltecApi.Repositories;
 using MiltecApi.Services;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseInMemoryDatabase("MiltecDb"));
+
 builder.Services.AddScoped<IAnuncioRepository, AnuncioRepository>();
 builder.Services.AddScoped<AnuncioService>();
 
-
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
